66928a52d3a14fa4865af06042bd0e9a2742ef2b
JohnDuxbury <jadux@yahoo.com>
2015-04-09 16:49:24 +0100

    MBString Upate

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -10,6 +10,10 @@ package "mysql-community-server" do
     action :install
 end
 
+package "php54w-mbstring.x86_64" do
+    action :install
+end
+
 service "mysqld" do
     action [:enable, :start]
 end
